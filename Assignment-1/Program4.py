380cd2d181036f3c2cdd7517ba493cd4ba08753d
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2021-02-07 19:48:29 +0530

    Add files via upload

---
+++ Assignment-1/Program4.py
@@ -0,0 +1,7 @@
+# Problem Statement :  Accept one number and check whether is is divisible by 5 or not.
+
+iNo = int(input("Enter a number\n"))
+if((iNo)%5==0):
+    print(iNo,"is divisible by 5")
+else:
+    print(iNo,"is not divisible by 5")
